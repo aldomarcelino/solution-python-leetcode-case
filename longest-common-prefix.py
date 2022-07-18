ef2cd214c12c17122b5153b7dfd2ac7a7c07d584
Aldo Marcelino <aldo115marcelino@gmail.com>
2022-07-18 09:43:52 +0700

    add function

--- longest-common-prefix.py
+++ longest-common-prefix.py
@@ -1,2 +1,15 @@
 def longestCommonPrefix(list):
-    pass
+    pre = ""
+    for i in range(len(list[0])):
+        for temp in list:
+            if i == len(temp) or temp[i] != list[0][i]:
+                return pre
+        pre += list[0][i]
+    return pre
+
+
+strs = ["flower", "flow", "flight"]
+
+for x in range(len(strs)):
+    print(strs[x])
+print(longestCommonPrefix(strs))
