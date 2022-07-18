3d74f2432e89b5bd98018ae1b7ae1b265122218c
Aldo Marcelino <aldo115marcelino@gmail.com>
2022-07-18 16:46:24 +0700

    open brackets must be closed by the same type

---
+++ valid-parent.py
@@ -0,0 +1,16 @@
+def isValid(s):
+    stack = []
+    closetoopen = {")": "(", "{": "}", "[": "]"}
+    for i in s:
+        if i in closetoopen:
+            if stack and stack[-1] == closetoopen[i]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(i)
+    return True if not stack else False
+
+
+s = "(())"
+print(isValid(s))
